9c0a01ec75cbc170575624e67e136af004a31245
®3¥m0&d <raymondjoaquin50@gmail.com>
2024-10-23 17:42:11 +0800

    Delete Const.cpp

--- Const.cpp
+++
@@ -1,8 +0,0 @@
-
-// const(You can use this if you don't want to change the data of variable forever as long as you dont fix the const)
-#include<iostream>
-int main() {
-    const int number = 10;
-    std::cout << number; 
-    return 0;
-}
